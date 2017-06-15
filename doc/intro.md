dcfbc0dee37247fa9f4b1c0f0cb401275f282b45
Anthon Taylor <anthonjtaylor@gmail.com>
2017-06-15 09:27:16 -0700

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to neato
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/neato/core.clj
@@ -0,0 +1,10 @@
+(ns neato.core
+  (:gen-class))
+
+(defn sup []
+  (println "Hey"))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
